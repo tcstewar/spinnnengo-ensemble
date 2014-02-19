ea1c049ed9bf002899bd56350375376d27c880b7
AndrewMundy <andrew.mundy@ieee.org>
2014-02-19 09:12:10 -0500

    Neatened code, added some additional initialisation.

--- ensemble-harness.c
+++ ensemble-harness.c
@@ -1,17 +1,21 @@
 #include "spin-nengo-ensemble.h"
 
-uint n_input_dimensions = 0;
-value_t * ibuf_accumulator = 0,
-        * ibuf_filtered = 0;
+// Parameters
+uint n_input_dimensions;  //! Number of input dimensions D_{in}
+uint n_output_dimensions; //! Number of output dimensions D_{out}
+uint n_neurons;           //! Number of neurons N
+value_t t_rc;             //! Membrane time constant
+value_t t_ref;            //! Refactory period 
 
-value_t * value_gains = 0;
-current_t * i_bias;
+current_t * i_bias;       //! Population biases : 1 x N
 
-accum * encoders;
-accum * decoders;
+accum * encoders; //! Encoder values : N x D_{in} (including gains)
+accum * decoders; //! Decoder values : N x SUM( d in D_{outs} )
 
-value_t * t_rc;
-value_t * t_ref;
+// Buffers
+value_t * ibuf_accumulator; //! Input buffers : 1 x D_{in}
+value_t * ibuf_filtered;    //! Filtered input buffers : 1 x D_{in}
+voltage_t * v_voltages;     //! Neuron voltages
 
 int c_main( void )
 {
@@ -32,7 +36,8 @@ int c_main( void )
   spin1_callback_on( MC_PACKET_RECEIVED, incoming_spike_callback, -1 );
   spin1_callback_on( TIMER_TICK, timer_callback, 2 );
 
-  io_printf( IO_STD, "Starting.\n" );
+  // Setup buffers, etc.
+  initialise_buffers( );
 
   // Go
   spin1_start( );
@@ -41,6 +46,13 @@ int c_main( void )
 //! Initialise buffers and values
 void initialise_buffers( void )
 {
-  ibuf_accumulator = spin1_malloc( sizeof( value_t ) * n_input_dimensions );
-  ibuf_filtered = spin1_malloc( sizeof( value_t ) * n_input_dimensions );
+  // Encoders / decoders / bias
+  encoders = spin1_malloc( sizeof(accum) * n_input_dimensions * n_neurons );
+  decoders = spin1_malloc( sizeof(accum) * n_output_dimensions * n_neurons );
+  i_bias = spin1_malloc( sizeof(current_t) * n_neurons );
+
+  // Input buffers / voltages
+  ibuf_accumulator = spin1_malloc( sizeof(value_t) * n_input_dimensions );
+  ibuf_filtered = spin1_malloc( sizeof(value_t) * n_input_dimensions );
+  v_voltages = spin1_malloc( sizeof(voltage_t) * n_neurons );
 }
--- spin-nengo-ensemble.h
+++ spin-nengo-ensemble.h
@@ -18,19 +18,19 @@ void incoming_spike_callback( uint key, uint payload );
 // Initialisation functions
 void initialise_buffers( void );
 
-// Buffers, variables
-extern uint n_input_dimensions; //! Number of input dimensions D_{in}
-extern uint n_neurons;          //! Number of neurons N
+// Parameters
+extern uint n_input_dimensions;  //! Number of input dimensions D_{in}
+extern uint n_output_dimensions; //! Number of output dimensions D_{out}
+extern uint n_neurons;           //! Number of neurons N
+extern value_t t_rc;             //! Membrane time constant
+extern value_t t_ref;            //! Refactory period 
 
-extern value_t * ibuf_accumulator; //! Input buffers : 1 x D_{in}
-extern value_t * ibuf_filtered;    //! Filtered input buffers : 1 x D_{in}
+extern current_t * i_bias;       //! Population biases : 1 x N
 
-extern value_t * value_gains; //! Population gains  : 1 x N
-extern current_t * i_bias;    //! Population biases : 1 x N
-
-extern accum * encoders; //! Encoder values : N x D_{in}
+extern accum * encoders; //! Encoder values : N x D_{in} (including gains)
 extern accum * decoders; //! Decoder values : N x SUM( d in D_{outs} )
 
-// Universal parameters
-extern value_t * t_rc;  //! Membrane time constant
-extern value_t * t_ref; //! Refactory period 
+// Buffers
+extern value_t * ibuf_accumulator; //! Input buffers : 1 x D_{in}
+extern value_t * ibuf_filtered;    //! Filtered input buffers : 1 x D_{in}
+extern voltage_t * v_voltages;     //! Neuron voltages
